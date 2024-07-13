6f6cf1599e4f8797781de411c70a849e90503292
gaurvjaiswal <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-13 14:09:00 +0530

    Add files via upload
    Setup and Day_01 challenge is completed.

---
+++ Day_01_Challenge/script.js
@@ -0,0 +1,57 @@
+//Task 1
+var number = prompt("Enter your number")
+number = Number.parseInt(number)
+console.log(number)
+
+//Task 2
+let name = " hello coders my name is gaurav "
+console.log(name)
+
+//Task 3
+const bool = true
+console.log(bool);
+
+console.log(typeof(bool))
+
+//Task 4
+
+
+  let num = 10
+  console.log(typeof(num));
+
+let name = "Gaurav"
+console.log(typeof(name));
+
+let bool = false;
+console.log(typeof(bool))
+
+let info = {
+    name : "gaurav",
+    age: 19,
+    year: "2nd year",
+    college: "GURUGRAM UNIVERSITY"
+
+}
+console.log(info)
+console.log(typeof(info))
+
+let skills = ["html", "css", "javascript", "react",]
+console.log(typeof(skills))
+
+
+//Task 5
+let name = "gaurav"
+let name = "rahul"
+
+console.log(name)
+
+
+
+//Task 6
+
+const age =  23;
+console.log(age)
+
+const age = 34;
+console.log(age)
+
