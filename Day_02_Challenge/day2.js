e88f38945419616ed027f62458e507db4eed401a
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-14 19:01:53 +0530

    Add files via upload

---
+++ Day_02_Challenge/day2.js
@@ -0,0 +1,105 @@
+// let a = 5;
+// let b = 6;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+ // Define the numbers
+// let number1 = 10;
+// let number2 = 3;
+
+// console.log(number1 % number2);
+
+// Task 2
+
+// let  num = 10;
+// num += 1;
+
+// console.log(num)
+
+// let  num = 10;
+// num -= 1;
+
+// console.log(num)
+
+
+
+// Task 3
+// let a = 10;
+// let b  = 5;
+
+// console.log(a>b);
+
+// Task 8
+// let a = 10;
+// let b  = 5;
+
+// console.log(a<b);
+
+
+// Task 9
+// let number1 = 10;
+// let number2 = 15;
+
+// if (number1 <= number2) {
+//     console.log(`${number1} is less than or equal to ${number2}`);
+// } else {
+//     console.log(`${number1} is not less than or equal to ${number2}`);
+// }
+
+// if (number1 >= number2) {
+//     console.log(`${number1} is greater than or equal to ${number2}`);
+// } else {
+//     console.log(`${number1} is not greater than or equal to ${number2}`);
+// }
+
+
+// Task 10
+// Define the numbers
+// let number1 = 10;
+// let number2 = "10";  
+
+// if (number1 == number2) {
+//     console.log("number 1 is equal to number 2");
+// } else {
+//     console.log("number is not equal to number 1");
+// } 
+
+// if (number1 === number2) {
+//     console.log("number 2 is strictly equal to number 1");
+// } else {
+//     console.log("Number1 is strictly not equal to number2");
+// }
+
+
+// Task11
+
+// let a = 5;
+// let b = 6;
+
+// let c = 7;
+
+// if(a == c && b == c) {
+//     console.log("Both a and b is equal to c");
+// }else{
+//     console.log("Both a and b are not equal to c");
+// }
+
+// let a = 5;
+// let b = 6;
+
+// let c = 5;
+
+// if(a == c || b == c) {
+//     console.log("Both a and b is equal to c");
+// }else{
+//     console.log("Both a and b are not equal to c");
+// }
+
+
+// // TASK 14
+// let number = -5;
+
+// let result = (number >= 0) ? "positive" : "negative";
+
+// console.log(`The number ${number} is ${result}.`);
