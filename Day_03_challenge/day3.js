1140fa1b231168f65a19c3640de81be96ad25bff
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-15 12:47:57 +0530

    Add files via upload

---
+++ Day_03_challenge/day3.js
@@ -0,0 +1,128 @@
+
+// TASK 1
+// let a = 0;
+
+// if(a > 0 ){
+//     console.log("a is a positive number")
+// }else if(a < 0){
+//     console.log("a is a negative number")
+// }else if(a == 0)
+// console.log("a is zero")
+
+// TASK 2
+
+// let a = parseInt(prompt("Enter your age"))
+
+// if(a >=18){
+//     console.log("your are eligible for voting")
+// }else
+// console.log("nunnu hai abhi tu")
+
+
+// TASK 3.
+
+// let a = 8;
+// let b = 4;
+// let c = 9;
+
+// if(a > b && a > c){
+//     console.log("a is greater")
+// }else if(b > a && b > c){
+//     console.log("b is greater")
+// }else
+// console.log("c is greater")
+
+// TASK 4
+
+// let a = parseInt(prompt("Enter day number"))
+
+// switch(a){
+//     case 1: 
+//     console.log("Monday")
+//     break;
+
+//     case 2: 
+//     console.log("tuesday")
+//     break;
+
+//     case 3: 
+//     console.log("webnesday")
+//     break;
+ 
+//     case 4: 
+//     console.log("thursday")
+//     break;
+ 
+//     case 5: 
+//     console.log("friday")
+//     break;
+
+//     case 6: 
+//     console.log("saturday")
+//     break;
+
+//     case 17: 
+//     console.log("sunday")
+//     break;
+
+//     default: 
+//     console.log("invalid number")
+//     break;
+   
+//  }
+
+// TASK 5
+
+// let a = parseInt(prompt("Enter marks"));
+
+// switch (true) {
+//     case (a >= 90):
+//         console.log("A grade");
+//         break;
+//     case (a < 90 && a >= 60):
+//         console.log("B grade");
+//         break;
+//     case (a < 60 && a >= 30):
+//         console.log("C grade");
+//         break;
+//     case (a < 30):
+//         console.log("FAIL");
+//         break;
+//     default:
+//         console.log("Invalid marks");
+// }
+
+
+// TASK 6
+// let number = parseInt(prompt("Enter a number"));
+
+// let result = (number % 2 === 0) ? "Even" : "Odd";
+
+// console.log(`The number ${number} is ${result}.`);
+
+// // TASK 7
+
+// let year = parseInt(prompt("Enter a year"));
+// let isLeapYear;
+
+// if (year % 4 == 0) {
+//     if (year % 100 == 0) {
+//         if (year % 400 == 0) {
+//             isLeapYear = true;
+//         } else {
+//             isLeapYear = false;
+//         }
+//     } else {
+//         isLeapYear = true;
+//     }
+// } else {
+//     isLeapYear = false;
+// }
+
+// if (isLeapYear) {
+//     console.log(`The year ${year} is a Leap year.`);
+// } else {
+//     console.log(`The year ${year} is Not a leap year.`);
+// }
+
+
