784fb98205db5d99b6d05371fb31faa962d622d7
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-16 11:24:25 +0530

    Add files via upload

---
+++ Day_04_challenge/day4.js
@@ -0,0 +1,60 @@
+// TASK 1
+// let n = parseInt(prompt("Enter the number"))
+// for(i=0; i<n; i++)
+// console.log(i)
+
+// // TASK 2
+// let n = parseInt(prompt("Enter the number"))
+// for(i=0; i<11; i++ )
+// console.log(i * 5)
+
+// // TASK 3
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+
+// console.log(`The sum of numbers from 1 to 10 is ${sum}`);
+
+
+
+// // TASK 4
+// let k = 10;
+// while (k >= 1) {
+//     console.log(k);
+//     k--;
+// }
+
+
+// TASK 5
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+// // TASK 6
+// let number = 5;   
+// let factorial = 1;
+// let i = 1;
+
+// do {
+//     factorial *= i;
+//     i++;
+// } while (i <= number);
+
+// console.log(`The factorial of ${number} is ${factorial}`);
+
+
+// TASK 7
+// let rows = 5;
+
+// for (let i = 1; i <= rows; i++) {
+//     let pattern = "";
+//     for (let j = 1; j <= i; j++) {
+//         pattern += "* ";
+//     }
+//     console.log(pattern.trim());
+// }
+
