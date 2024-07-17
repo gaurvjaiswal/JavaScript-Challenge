813332474c92a0c3a36a4d19b5f3962c6be9d7b4
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-17 10:45:52 +0530

    Add files via upload

---
+++ Day_05_challenge/script.js
@@ -0,0 +1,119 @@
+//task 1
+
+// function isEvenodd(number){
+//     if(number % 2 === 0){
+//         return "Even";
+//     }else {
+//         return "odd";
+//     }
+// }
+
+// let number = parseInt(prompt("Enter the number"))
+// console.log(number + "is" + isEvenodd(number));
+
+
+//Task 2
+
+// function square(number){    
+//     return number * number;
+// }
+
+// let number = parseInt(prompt("Enter the number"))
+// console.log("sqaure of " + number + "is" + "  " + square(number))
+
+// //Task 3
+// const findMax = function(a, b) {
+//     return a > b ? a : b;
+//   }
+ 
+//   let num1 = 7;
+//   let num2 = 4;
+//   console.log("The maximum between " + num1 + " and " + num2 + " is " + findMax(num1, num2)
+
+//Task 4
+// const concatenateStrings = function(string1, string2) {
+//     return string1 + string2;
+//   };
+  
+   
+//   let str1 = "Gaurav  ";
+//   let str2 = "Sain";
+//   console.log(concatenateStrings(str1, str2));   
+   
+//Task 5
+// const sum = (a, b) => a + b;
+// let num1 = 5;
+// let num2 = 3;
+// console.log("The sum of " + num1 + " and " + num2 + " is " + sum(num1, num2));   
+
+
+//Task 6
+// const containsCharacter = (str, char) => str.includes(char);
+// let str = "Hello, world!";
+// let char = 'w';
+// console.log(`Does the string "${str}" contain the character '${char}'? ${containsCharacter(str, char)}`);   
+
+ 
+// // Task 7
+// function multiply(a, b = 1) {
+//     return a * b;
+//   }
+  
+//   let num1 = 5;
+//   let num2 = 3;
+//   console.log(`The product of ${num1} and ${num2} is ${multiply(num1, num2)}`);   
+   
+  
+ 
+// //Task 8
+// function greet(name, age) {
+//     return `Hello, ${name}! You are ${age} years old.`;
+//   }
+  
+//   // Example usage:
+//   let name = "Gaurav";
+//   let age = 19;
+//   console.log(greet(name, age));  
+
+// Task  9
+// function repeatFunction(fn, times) {
+//     for (let i = 0; i < times; i++) {
+//       fn();
+//     }
+//   }
+ 
+
+//   const sayHello = () => {
+//     console.log("Hello!");
+//   };
+  
+//   repeatFunction(sayHello, 3);   
+
+
+// //Task 10
+// function applyFunctions(func1, func2, value) {
+//     const result1 = func1(value);
+//     const result2 = func2(result1);
+//     return result2;
+//   }
+  
+//   const double = (x) => x * 2;
+//   const square = (x) => x * x;
+  
+//   const value = 5;
+//   const finalResult = applyFunctions(double, square, value);
+  
+//   console.log(`Result of applying double and then square to ${value} is: ${finalResult}`); 
+  
+  
+
+
+
+
+
+
+
+
+ 
+   
+  
