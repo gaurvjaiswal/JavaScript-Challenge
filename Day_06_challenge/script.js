1d16e7f070dc5e0e7fc99f1046e974fba079d9b2
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-18 16:49:37 +0530

    Add files via upload

---
+++ Day_06_challenge/script.js
@@ -0,0 +1,99 @@
+//Task 1
+// let arr = [1,2,3,5,6]
+// console.log(arr)
+
+// TASK 2
+// let array = [10, 20, 30, 40, 50];
+// let firstElement = array[0];
+// let lastElement = array[array.length - 1];
+// console.log("First Element:", firstElement); 
+// console.log("Last Element:", lastElement);   
+
+
+// TASK 3
+// let array = [10, 20, 30, 40, 50];
+// array.push(60);
+// console.log(array);
+
+
+// // TASK 4
+// let array = [10, 20, 30, 40, 50];
+// let lastElement = array.pop();
+//console.log(lastElement); 
+// console.log(array);       
+
+// task 5
+// let array = [10, 20, 30, 40, 50];
+// aray.unshift(0);
+// console.log(array); 
+ 
+
+// TASK 6
+// let array = [10, 20, 30, 40, 50];
+// let firstElement = array.shift();
+// console.log(firstElement); 
+// console.log(array);       
+
+// Task 7
+// let array = [10, 20, 30, 40, 50];
+// let doubledArray = array.map(function(number) {
+//   return number * 2;
+// });
+
+// console.log(doubledArray)
+
+// // TASK 8
+// let array = [10, 15, 20, 25, 30, 35, 40];
+
+// let evenArray = array.filter(function(number) {
+//   return number % 2 === 0;
+// });
+
+// console.log(evenArray); 
+
+// TASK 9
+// let array = [10, 20, 30, 40, 50];
+
+// let sum = array.reduce(function(accumulator, currentValue) {
+//   return accumulator + currentValue;
+// }, 0);
+
+// console.log(sum); 
+
+
+// TASK 10
+// let array = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+
+// TASK 11
+// let array = [10, 20, 30, 40, 50];
+
+// array.forEach(function(element) {
+//   console.log(element);
+// });
+
+
+// TASK 12
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ];
+  
+//   console.log(matrix);
+  
+
+
+// TASK 13
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ];
+  
+//   let element = matrix[1][2];
+//   console.log(element); 
