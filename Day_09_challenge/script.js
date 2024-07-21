fa4f15aac773ae71cacc3fc8a730167a4b3ca1a3
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-21 13:33:12 +0530

    Add files via upload

---
+++ Day_09_challenge/script.js
@@ -0,0 +1,84 @@
+// // TASK 1
+// document.querySelector("#text").innerHTML = "HELLO";
+
+// TASK 2
+// var a = document.querySelector(".text")
+// a.style.backgroundColor = "red"
+
+
+// // TASK 3
+//  var newDiv = document.createElement('div');
+        
+//  newDiv.textContent = 'This is a newly created div';
+//  document.body.appendChild(newDiv);
+
+// // TASK 4
+//   var newListItem = document.createElement('li');
+        
+//   newListItem.textContent = 'New Item';
+//   var ulElement = document.getElementById('myList');
+  
+//   ulElement.appendChild(newListItem);
+
+
+// TASK 5
+// function removeElement() {
+//     var element = document.getElementById('elementToRemove');
+    
+//     element.remove();
+// }
+
+
+
+// // TASK 6
+// function removeLastChild() {
+//     var parentElement = document.getElementById('myList');
+    
+//     parentElement.removeChild(parentElement.lastElementChild);
+// }
+
+
+// // TASK 7
+// function changeImageSrc() {
+//     var imgElement = document.getElementById('myImage');
+    
+//     imgElement.setAttribute('src', 'new-image.png');
+    
+// }
+
+
+
+// // TASK 8
+// function addClass() {
+//     var element = document.getElementById('myParagraph');
+    
+//     element.classList.add('highlight');
+// }
+
+// function removeClass() {
+//     var element = document.getElementById('myParagraph');
+    
+//     element.classList.remove('highlight');
+// }
+
+
+
+// TASK 9
+//   var button = document.getElementById('myButton');
+//   var paragraph = document.getElementById('myParagraph');
+
+//   button.addEventListener('click', function() {
+//       paragraph.textContent = 'The text has been changed!';
+//   });.
+// 
+
+// TASK 10
+// const box = document.querySelector('.box');
+
+// box.addEventListener('mouseover', function() {
+//     box.style.borderColor = 'red';
+// });
+
+// box.addEventListener('mouseout', function() {
+//     box.style.borderColor = 'black';
+// });
