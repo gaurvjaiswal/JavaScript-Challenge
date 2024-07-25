4e6b90515759bc1dc8cf5cb823942ec4a5b0454c
Gaurva sain <142992086+gaurvjaiswal@users.noreply.github.com>
2024-07-25 10:03:23 +0530

    Add files via upload
    Day_13_challenge completed

---
+++ Day_13_challenge/script.js
@@ -0,0 +1,73 @@
+//  TASK 1
+
+// const addNumbers = require('./script2');
+
+// const num1 = 5;
+// const num2 = 3;
+// const result = addNumbers(num1, num2);
+// console.log(`The sum of ${num1} and ${num2} is ${result}`);
+
+
+// TASK 2
+// main.js
+
+// const person = require('./script2');
+
+// console.log(person.fullName());  
+// console.log(person.greet()); 
+
+
+// TASK 3
+ 
+
+// const { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers } = require('./script2');
+// const num1 = 10;
+// const num2 = 5;
+// console.log(`The sum of ${num1} and ${num2} is ${addNumbers(num1, num2)}`);
+// console.log(`The difference between ${num1} and ${num2} is ${subtractNumbers(num1, num2)}`);
+// console.log(`The product of ${num1} and ${num2} is ${multiplyNumbers(num1, num2)}`);
+// console.log(`The quotient of ${num1} divided by ${num2} is ${divideNumbers(num1, num2)}`
+
+
+// TASK 4
+
+
+// import addNumbers from './script2';
+
+// const num1 = 5;
+// const num2 = 3;
+// const result = addNumbers(num1, num2);
+// console.log(`The sum of ${num1} and ${num2} is ${result}`);
+
+// TASK 5
+ 
+
+// import * as utilities from './script2';
+
+// const num1 = 10;
+// const num2 = 5;
+
+// console.log(`PI: ${utilities.PI}`);
+// console.log(`E: ${utilities.E}`);
+// console.log(`The sum of ${num1} and ${num2} is ${utilities.addNumbers(num1, num2)}`);
+// console.log(`The difference between ${num1} and ${num2} is ${utilities.subtractNumbers(num1, num2)}`);
+// console.log(`The product of ${num1} and ${num2} is ${utilities.multiplyNumbers(num1, num2)}`);
+// console.log(`The quotient of ${num1} divided by ${num2} is ${utilities.divideNumbers(num1, num2)}`);
+
+// task 6
+
+
+// import _ from 'lodash';
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // Use the _.shuffle function to shuffle the array
+// const shuffledNumbers = _.shuffle(numbers);
+
+// console.log('Original numbers:', numbers);
+// console.log('Shuffled numbers:', shuffledNumbers);
+
+
+
+
+// Task 7
---
+++ Day_13_challenge/script2.js
@@ -0,0 +1,80 @@
+//  task 1
+
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// module.exports = addNumbers;
+
+// TASk 2 
+// const person = {
+//     firstName: 'Gaurav',
+//     lastName: 'sain',
+//     age: 19,
+//     fullName: function() {
+//         return `${this.firstName} ${this.lastName}`;
+//     },
+//     greet: function() {
+//         return `Hello, my name is ${this.fullName()} and I am ${this.age} years old.`;
+//     }
+// };
+
+// module.exports = person;
+
+// TASK 3
+
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+// function subtractNumbers(a, b) {
+//     return a - b;
+// }
+// function multiplyNumbers(a, b) {
+//     return a * b;
+// }
+// function divideNumbers(a, b) {
+//     if (b === 0) {
+//         throw new Error("Cannot divide by zero");
+//     }
+//     return a / b;
+// }
+// module.exports = { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers };
+
+// TASK 4
+ 
+
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// export default addNumbers;
+
+
+// TASk 5.
+ 
+
+// export const PI = 3.14159;
+// export const E = 2.71828;
+
+// export function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// export function subtractNumbers(a, b) {
+//     return a - b;
+// }
+
+// export function multiplyNumbers(a, b) {
+//     return a * b;
+// }
+
+// export function divideNumbers(a, b) {
+//     if (b === 0) {
+//         throw new Error("Cannot divide by zero");
+//     }
+//     return a / b;
+// }
+
+
+
+// TASK 6
